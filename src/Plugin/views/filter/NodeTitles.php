17eab4365373987dd93603004f2432eef0e24ce8
upchuk <daniel.sipos@wunderkraut.com>
2015-01-24 17:32:03 +0100

    Creating two custom filters

---
+++ src/Plugin/views/filter/NodeTitles.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @file
+ * Definition of Drupal\d8views\Plugin\views\filter\NodeTitles.
+ */
+
+namespace Drupal\d8views\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\display\DisplayPluginBase;
+use Drupal\views\Plugin\views\filter\InOperator;
+use Drupal\views\ViewExecutable;
+
+/**
+ * Filters by node type containing string.
+ *
+ * @ingroup views_filter_handlers
+ *
+ * @ViewsFilter("d8views_node_titles")
+ */
+class NodeTitles extends InOperator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(ViewExecutable $view, DisplayPluginBase $display, array &$options = NULL) {
+    parent::init($view, $display, $options);
+    $this->valueTitle = t('Allowed node titles');
+    $this->definition['options callback'] = array($this, 'generateOptions');
+  }
+
+  /**
+   * Override the query so that no filtering takes place if the user doesn't
+   * select any options.
+   */
+  public function query() {
+    if (!empty($this->value)) {
+      parent::query();
+    }
+  }
+
+  /**
+   * Skip validation if no options have been chosen so we can use it as a
+   * non-filter.
+   */
+  public function validate() {
+    if (!empty($this->value)) {
+      parent::validate();
+    }
+  }
+
+  /**
+   * Helper function that generates the options.
+   * @return array
+   */
+  public function generateOptions() {
+    // Array keys are used to compare with the table field values.
+    return array(
+      'my title' => 'my title',
+      'another title' => 'another title',
+    );
+  }
+
+}
